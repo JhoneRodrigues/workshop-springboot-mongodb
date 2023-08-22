285026564daba60efc3b1b35b809fbb8430542f5
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-08-22 15:47:44 -0300

    Simple query with @Query

--- src/main/java/com/jhonerodrigues/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/jhonerodrigues/workshopmongo/repositories/PostRepository.java
@@ -3,11 +3,15 @@ package com.jhonerodrigues.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.jhonerodrigues.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List <Post> findByTitleContainingIgnoreCase (String text);
 }
